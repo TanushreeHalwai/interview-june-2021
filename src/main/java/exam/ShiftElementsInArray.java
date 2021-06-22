2888060153a0943c41538f21c6eb7560ef7f66bc
TanushreeHalwai <86236434+TanushreeHalwai@users.noreply.github.com>
2021-06-22 12:58:13 +0530

    Update ShiftElementsInArray.java

--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -4,7 +4,41 @@ public class ShiftElementsInArray {
 
   public void shift(int[] arr) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+int n=1;
+for(int i = 0; i < n; i++){    
+            int j, last;    
+            //Stores the last element of array    
+            last = arr[arr.length-1];    
+            
+            for(j = arr.length-1; j > 0; j--){    
+                //Shift element of array by one    
+                arr[j] = arr[j-1];    
+            }    
+            //Last element of array will be added to the start of array.    
+            arr[0] = last;    
+        }    
+        
+        
+        
+    
+        System.out.println();   
+         
+            
+        //Displays resulting array after rotation    
+        System.out.println("Array after right rotation: ");    
+        for(int i = 0; i< arr.length; i++){   
+           System.out.print(arr[i] + " ");    
+        }   
+}
 
-  }
+public static void main(String[] args)
+    {
+        ShiftElementsInArray rotate = new ShiftElementsInArray();
+        int arr[] = { 1, 2, 3, 4, 5 };
+        rotate.shift(arr);
+        
+    }
+  
+  
 
 }
